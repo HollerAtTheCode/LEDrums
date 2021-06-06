0ec718a15d65ed2c6f5f4510b9b4b6797a34cda0
holler_at_the_code <thelimbslasher@googlemail.com>
2021-06-06 19:11:18 +0200

    functioning drum lights

--- midi/MIDI_Input.ino/MIDI_Input.ino.ino
+++ midi/MIDI_Input.ino/MIDI_Input.ino.ino
@@ -13,16 +13,16 @@
 #define LED_COUNT_RACK 20
 
 #define LED_PIN_SNARE 5
-//#define LED_PIN_TOM1 6
-//#define LED_PIN_TOM2 7
-//#define LED_PIN_TOM3 8
-//#define LED_PIN_RACK 9
+#define LED_PIN_TOM1 6
+#define LED_PIN_TOM2 7
+#define LED_PIN_TOM3 8
+#define LED_PIN_RACK 9
 
 Adafruit_NeoPixel snareLeds = Adafruit_NeoPixel(LED_COUNT_SNARE, LED_PIN_SNARE, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel tom1Leds(LED_COUNT_TOM, LED_PIN_TOM1, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel tom2Leds(LED_COUNT_TOM, LED_PIN_TOM2, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel tom3Leds(LED_COUNT_TOM, LED_PIN_TOM3, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel rackLeds(LED_COUNT_RACK, LED_PIN_RACK, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel tom1Leds = Adafruit_NeoPixel(LED_COUNT_TOM, LED_PIN_TOM1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel tom2Leds = Adafruit_NeoPixel(LED_COUNT_TOM, LED_PIN_TOM2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel tom3Leds = Adafruit_NeoPixel(LED_COUNT_TOM, LED_PIN_TOM3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel rackLeds = Adafruit_NeoPixel(LED_COUNT_RACK, LED_PIN_RACK, NEO_GRB + NEO_KHZ800);
 
 // Drum Mappings
 #define HIHAT_TOP 46
@@ -63,20 +63,35 @@ void MyHandleNoteOn(byte channel, byte pitch, byte velocity) {
   }
   if (pitch == SNARE || pitch == SNARE_RIM || pitch == SNARE_RIM2) {
     Serial.print("Snare");
-    snareLeds.setBrightness(100);
+    snareLeds.setBrightness(velocity);
     snareLeds.show();
-    delay(50);
+    delay(100);
     snareLeds.setBrightness(5);
     snareLeds.show();
   }
   else if (pitch == TOM1 || pitch == TOM1_RIM) {
     Serial.print("TOM1");
+    tom1Leds.setBrightness(velocity);
+    tom1Leds.show();
+    delay(100);
+    tom1Leds.setBrightness(5);
+    tom1Leds.show();
   }
   else if (pitch == TOM2 || pitch == TOM2_RIM) {
     Serial.print("TOM2");
+    tom2Leds.setBrightness(velocity);
+    tom2Leds.show();
+    delay(100);
+    tom2Leds.setBrightness(5);
+    tom2Leds.show();
   }
   else if (pitch == TOM3 || pitch == TOM3_RIM) {
     Serial.print("TOM3");
+    tom3Leds.setBrightness(velocity);
+    tom3Leds.show();
+    delay(100);
+    tom3Leds.setBrightness(5);
+    tom3Leds.show();
   }
   else if (pitch == CRASH_TOP || pitch == CRASH_RIM) {
     Serial.print("Crash");
@@ -98,22 +113,34 @@ void setup() {
 
   snareLeds.begin();
   snareLeds.setBrightness(5);
-  colorWipe(snareLeds.Color(66, 239, 245), 50);
+  colorWipe(&snareLeds, snareLeds.Color(166, 32, 106), 50, LED_COUNT_SNARE);
   snareLeds.show();
   
-  //tom1Leds.begin();
-  //tom2Leds.begin();
-  //tom3Leds.begin();
+  tom1Leds.begin();
+  tom1Leds.setBrightness(5);
+  colorWipe(&tom1Leds, tom1Leds.Color(241, 106, 67), 50, LED_COUNT_TOM);
+  tom1Leds.show();
+
+  tom2Leds.begin();
+  tom2Leds.setBrightness(5);
+  colorWipe(&tom2Leds, tom2Leds.Color(247, 217, 105), 50, LED_COUNT_TOM);
+  tom2Leds.show();
+  
+  tom3Leds.begin();
+  tom3Leds.setBrightness(5);
+  colorWipe(&tom3Leds, tom3Leds.Color(47, 147, 149), 50, LED_COUNT_TOM);
+  tom3Leds.show();
+  
   //rackLeds.begin();
   pinMode (LED, OUTPUT);
   MIDI.begin(10);
   MIDI.setHandleNoteOn(MyHandleNoteOn);
 }
 
-void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<LED_COUNT_SNARE; i++) { // For each pixel in strip...
-    snareLeds.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    snareLeds.show();                          //  Update strip to match
+void colorWipe(Adafruit_NeoPixel *ledStrip, uint32_t color, int wait, int ledCount) {
+  for(int i=0; i<ledCount; i++) { // For each pixel in strip...
+    ledStrip->setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    ledStrip->show();                          //  Update strip to match
     delay(wait);                           //  Pause for a moment
   }
 }
